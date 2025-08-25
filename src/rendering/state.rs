da3ee4a6676b8c5f078ffd36cc8ddc1e83bb5c10
TonyStr <hello@tonystr.net>
2025-08-25 15:23:45 +0200

    feat: render texture & move camera

--- src/rendering/state.rs
+++ src/rendering/state.rs
@@ -1,9 +1,9 @@
 use std::sync::Arc;
-use winit::{ event_loop::ActiveEventLoop, keyboard::KeyCode, window::Window };
+use winit::{ event::WindowEvent, event_loop::ActiveEventLoop, keyboard::KeyCode, window::Window };
 use bytemuck::{Pod, Zeroable};
 use wgpu::util::DeviceExt;
 
-use crate::texture::Texture;
+use crate::{rendering::camera::{Camera, CameraController, CameraUniform}, texture::Texture};
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Pod, Zeroable)]
@@ -46,7 +46,7 @@ const INDICES: &[u16] = &[
     1, 2, 4,
     2, 3, 4,
 ];
- 
+
 pub struct State {
     pub surface: wgpu::Surface<'static>,
     pub device: wgpu::Device,
@@ -61,6 +61,11 @@ pub struct State {
     pub num_indices: u32,
     pub diffuse_bind_group: wgpu::BindGroup,
     pub diffuse_texture: Texture,
+    pub camera: Camera,
+    pub camera_uniform: CameraUniform,
+    pub camera_buffer: wgpu::Buffer,
+    pub camera_bind_group: wgpu::BindGroup,
+    pub camera_controller: CameraController,
 }
 
 impl State {
@@ -114,9 +119,6 @@ impl State {
 
         let diffuse_bytes = include_bytes!("../../happy-tree.png");
         let diffuse_texture = Texture::from_bytes(&device, &queue, diffuse_bytes, Some("happy-tree.png")).unwrap();
-        // let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
-        // let diffuse_rgba = diffuse_image.to_rgba8();
-
 
         let texture_bind_group_layout = device.create_bind_group_layout(
             &wgpu::BindGroupLayoutDescriptor {
@@ -157,11 +159,59 @@ impl State {
             ],
         });
 
+        let camera = Camera {
+            eye: (0.0, 1.0, 2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: config.width as f32 / config.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+
+        let mut camera_uniform = CameraUniform::new();
+        camera_uniform.update_view_proj(&camera);
+
+        let camera_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Camera Buffer"),
+            contents: bytemuck::cast_slice(&[camera_uniform]),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let camera_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("camera_bind_group_layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }
+            ],
+        });
+
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("camera_bind_group"),
+            layout: &camera_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: camera_buffer.as_entire_binding()
+                }
+            ],
+        });
 
         let shader = device.create_shader_module(wgpu::include_wgsl!("../shader.wgsl"));
         let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
-            bind_group_layouts: &[&texture_bind_group_layout],
+            bind_group_layouts: &[
+                &texture_bind_group_layout,
+                &camera_bind_group_layout,
+            ],
             push_constant_ranges: &[],
         });
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
@@ -216,6 +266,8 @@ impl State {
 
         let num_indices = INDICES.len() as u32;
 
+        let camera_controller = CameraController::new(0.2);
+
         Ok(Self {
             surface,
             device,
@@ -235,6 +287,11 @@ impl State {
             num_indices,
             diffuse_bind_group,
             diffuse_texture,
+            camera,
+            camera_uniform,
+            camera_buffer,
+            camera_bind_group,
+            camera_controller,
         })
     }
 
@@ -280,6 +337,7 @@ impl State {
 
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+            render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
 
@@ -293,13 +351,20 @@ impl State {
     }
 
 
-    pub fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+    pub fn handle_key(&mut self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
         match (code, is_pressed) {
             (KeyCode::Escape, true) => event_loop.exit(),
-            _ => {}
+            _ => { self.camera_controller.handle_key(event_loop, code, is_pressed); }
         }
     }
 
     pub fn update(&mut self) {
+        self.camera_controller.update_camera(&mut self.camera);
+        self.camera_uniform.update_view_proj(&self.camera);
+        self.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
     }
+
+    // pub fn input(&mut self, event: &WindowEvent) -> bool {
+    //     self.camera_controller.process_events(event)
+    // }
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,5 +1,12 @@
 // Vertex shader
 
+struct CameraUniform {
+	view_proj: mat4x4<f32>,
+};
+
+@group(1) @binding(0)
+var<uniform> camera: CameraUniform;
+
 struct VertexInput {
 	@location(0) pos: vec3<f32>,
 	@location(1) tex_coords: vec2<f32>,
@@ -16,7 +23,7 @@ fn vs_main(
 ) -> VertexOutput {
 	var out: VertexOutput;
 	out.tex_coords = in.tex_coords;
-	out.clip_pos = vec4<f32>(in.pos, 1.0);
+	out.clip_pos = camera.view_proj * vec4<f32>(in.pos, 1.0);
 	return out;
 }
 
